2e5bfbde52442ee3f57b8311ba10498eef2d00d2
sergeynikol <104057963+sergeynikol@users.noreply.github.com>
2023-02-23 18:24:24 +0400

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -9,7 +9,16 @@ python3 -m venv venv
 echo "Executing App in '$BASEDIR'"
 # aктивирует виртуальное окружение, находящееся в директории venv
 source $BASEDIR/venv/bin/activate
-
+# 
 python3 -m pip install pip-tools
 
-python $BASEDIR/rtvt.py
+git clone https://github.com/sergeynikol/quick-project-start.git
+
+mv quick-project-start/.gitignore .gitignore
+
+# mv quick-project-start/settings.json settings.json
+
+# mv quick-project-start/.gitignore .gitignore
+
+python $BASEDIR/quick-project-start/rtvt.py
+
